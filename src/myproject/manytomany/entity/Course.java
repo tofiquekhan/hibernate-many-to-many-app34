78604a84fad66cbb3135769279f8b4f50ea1f299
Tofique Ahmed  Khan <tofique@webkorps.com>
2022-04-18 15:16:39 +0530

    Many to Many using annotation

--- src/myproject/manytomany/entity/Course.java
+++ src/myproject/manytomany/entity/Course.java
@@ -1,9 +1,21 @@
 package myproject.manytomany.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course4")
+
 public class Course {
 
+	@Id
+	@Column(length = 5)
 	private String cid;
+	@Column(length = 10)
 	private String cname;
+	@Column(length = 5)
 	private int ccost;
 	public String getCid() {
 		return cid;
--- src/myproject/manytomany/entity/Student.java
+++ src/myproject/manytomany/entity/Student.java
@@ -2,11 +2,29 @@ package myproject.manytomany.entity;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Student4")
 public class Student {
 
+	@Id
+	@Column(length = 5)
 	private String sid;
+	@Column(length = 10)
 	private String sname;
+	@Column(length = 10)
 	private String saddr;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "student4_course_4",joinColumns = {@JoinColumn(name="sid")}, inverseJoinColumns = {@JoinColumn(name="cid")})
+	
 	private Set<Course> courses;
 	public String getSid() {
 		return sid;
