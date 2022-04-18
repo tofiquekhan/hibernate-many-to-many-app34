e1176511e32854639f6fe71d648c5c851aa82350
Tofique Ahmed  Khan <tofique@webkorps.com>
2022-04-18 14:56:15 +0530

    Many to many using XML File

---
+++ src/myproject/manytomany/entity/Course.java
@@ -0,0 +1,28 @@
+package myproject.manytomany.entity;
+
+public class Course {
+
+	private String cid;
+	private String cname;
+	private int ccost;
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public int getCcost() {
+		return ccost;
+	}
+	public void setCcost(int ccost) {
+		this.ccost = ccost;
+	}
+	
+	
+}
---
+++ src/myproject/manytomany/entity/Student.java
@@ -0,0 +1,37 @@
+package myproject.manytomany.entity;
+
+import java.util.Set;
+
+public class Student {
+
+	private String sid;
+	private String sname;
+	private String saddr;
+	private Set<Course> courses;
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSaddr() {
+		return saddr;
+	}
+	public void setSaddr(String saddr) {
+		this.saddr = saddr;
+	}
+	public Set<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+	
+	
+}
